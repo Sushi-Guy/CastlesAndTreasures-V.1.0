a130613e7daaa64e2e1898fa9fea978b004ce785
Sushi-Guy <155205919+Sushi-Guy@users.noreply.github.com>
2024-05-11 13:55:10 -0400

    starting world save & load

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
+#include "headers/load.h"
 
 // Variables & constants avalible to all of main.cpp
 std::string noStop;
+std::ifstream loadWorld;
+std::ofstream makeWorld;
+
+// Functions
+void clearNR() {
+    for(int i = 0; i < 51; i++) {
+        std::cout << "\n";
+    }
+}
 
 int main() {
+    std::cout << "Loading...\n";
+    
+
     std::cout << "### ### ### ### #   ### ###        \n#   # # #    #  #   #   #          \n#   ### ###  #  #   ##  ###        \n#   # #   #  #  #   #     #        \n### # # ###  #  ### ### ###        \n\n                                 #### #  # ###                      \n#### ## # #  #                     \n#  # # ## ###                      \n\n### ### ### ### ### # # ### ### ###\n #  # # #   # # #   # # # # #   #  \n #  ### ##  ### ### # # ### ##  ###\n #  ##  #   # #   # # # ##  #     #\n #  # # ### # # ### ### # # ### ###\n\n # #  #    ####                    \n # # ##    ## #                    \n # #  #    ## #                    \n # #  #    # ##                    \n  #  ### # ####                    \n";
     
     std::cout << "Enter any key(s) to exit...";
